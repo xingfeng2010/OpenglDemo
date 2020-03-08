1d7f2ac9f86c84aa9f29b81dd118ced606c54ebc
xingfeng2010 <583354000@qq.com>
2020-03-08 20:34:18 +0800

    update sheader

--- app/src/main/res/raw/show_screen_vertex_shader.glsl
+++ app/src/main/res/raw/show_screen_vertex_shader.glsl
@@ -5,7 +5,7 @@ varying vec2 textureCoordinate;//纹理位置  与fragment_shader交互
 uniform mat4 u_Matrix;
 
 void main() {
+    gl_Position = u_Matrix*a_Position;
     textureCoordinate = a_TextureCoordinates;
-    gl_Position = a_Position;
 }
 
